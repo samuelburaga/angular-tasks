c9da8af0d62345f0a56ae5aace2bce9cb3de4a8d
Samuel Buraga <116203173+samuelburaga@users.noreply.github.com>
2023-07-17 20:04:22 +0300

    resolved cr 1

--- src/app/components/text-input/text-input.component.ts
+++ src/app/components/text-input/text-input.component.ts
@@ -3,10 +3,8 @@ import { Component } from "@angular/core";
 @Component({
 	selector: "app-text-input",
 	templateUrl: "./text-input.component.html",
-	styleUrls: ["./text-input.component.css"],
 })
 export class TextInputComponent {
-	Name: string = "";
 	message: string = "";
 	isDisabled: boolean = true;
 
@@ -17,9 +15,5 @@ export class TextInputComponent {
 	disable() {
 		this.isDisabled = true;
 		this.message = "";
-	}
-
-	printMessage() {
-		this.message = this.Name;
 	}
 }
