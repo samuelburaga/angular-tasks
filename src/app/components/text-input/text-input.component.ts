3497122f87aa08f7ed18956d9ebc0e6794a33364
Samuel Buraga <116203173+samuelburaga@users.noreply.github.com>
2023-07-17 15:41:44 +0300

    'Enable' effect works

--- src/app/components/text-input/text-input.component.ts
+++ src/app/components/text-input/text-input.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-text-input',
-  templateUrl: './text-input.component.html',
-  styleUrls: ['./text-input.component.css']
+	selector: "app-text-input",
+	templateUrl: "./text-input.component.html",
+	styleUrls: ["./text-input.component.css"],
 })
 export class TextInputComponent {
-
+	Name: string = "";
+	message: string = "";
+	enable() {
+		(<HTMLInputElement>document.getElementById("input")).disabled = false;
+	}
 }
