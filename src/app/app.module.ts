139a2d7244cca47b20270ab379f22f6221ed4ea3
Samuel Buraga <116203173+samuelburaga@users.noreply.github.com>
2023-07-18 12:43:34 +0300

    created card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { TextInputComponent } from "./components/text-input/text-input.component
 import { FormsModule } from "@angular/forms";
 import { ListComponent } from "./components/list/list.component";
 import { TasksComponent } from './components/tasks/tasks.component';
+import { CardComponent } from './components/tasks/card/card.component';
 
 @NgModule({
-	declarations: [AppComponent, ButtonComponent, TextInputComponent, ListComponent, TasksComponent],
+	declarations: [AppComponent, ButtonComponent, TextInputComponent, ListComponent, TasksComponent, CardComponent],
 	imports: [BrowserModule, FormsModule],
 	providers: [],
 	bootstrap: [AppComponent],
