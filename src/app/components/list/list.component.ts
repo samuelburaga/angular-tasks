ceaa5b1e6b6fa57939736e3a49c6d82be28df443
Samuel Buraga <116203173+samuelburaga@users.noreply.github.com>
2023-07-18 10:25:20 +0300

    my name is colored red

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -3,13 +3,17 @@ import { Component } from "@angular/core";
 @Component({
 	selector: "app-list",
 	templateUrl: "./list.component.html",
-	styleUrls: ["./list.component.css"],
 })
 export class ListComponent {
 	students: string[] = ["Valentina", "Elisei", "Sami", "Darian"];
 	isHidden: boolean = false;
+	color: string = "black";
 
 	changeStatus() {
 		this.isHidden = !this.isHidden;
+	}
+
+	isMe(student: string) {
+		return student === "Sami";
 	}
 }
